c1cff091b6cad98e8f3fd4d96bd580013de8eec4
Ivan Goncharov <ivan.goncharov@easybrain.com>
2019-10-04 10:48:05 +0300

    Google - Utilize all AdSizes in banner size calculation

--- AdMob/src/main/java/com/mopub/mobileads/GooglePlayServicesBanner.java
+++ AdMob/src/main/java/com/mopub/mobileads/GooglePlayServicesBanner.java
@@ -19,8 +19,10 @@ import java.util.Map;
 
 import static com.google.android.gms.ads.AdSize.BANNER;
 import static com.google.android.gms.ads.AdSize.FULL_BANNER;
+import static com.google.android.gms.ads.AdSize.LARGE_BANNER;
 import static com.google.android.gms.ads.AdSize.LEADERBOARD;
 import static com.google.android.gms.ads.AdSize.MEDIUM_RECTANGLE;
+import static com.google.android.gms.ads.AdSize.WIDE_SKYSCRAPER;
 import static com.google.android.gms.ads.RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_FALSE;
 import static com.google.android.gms.ads.RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE;
 import static com.google.android.gms.ads.RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_UNSPECIFIED;
@@ -185,8 +187,12 @@ public class GooglePlayServicesBanner extends CustomEventBanner {
 
     private static AdSize calculateAdSize(int width, int height) {
         // Use the largest AdSize that fits into MoPubView
-        if (height >= MEDIUM_RECTANGLE.getHeight() && width >= MEDIUM_RECTANGLE.getWidth()) {
+        if (height >= WIDE_SKYSCRAPER.getHeight() && width >= WIDE_SKYSCRAPER.getWidth()) {
+            return WIDE_SKYSCRAPER;
+        } else if (height >= MEDIUM_RECTANGLE.getHeight() && width >= MEDIUM_RECTANGLE.getWidth()) {
             return MEDIUM_RECTANGLE;
+        } else if (height >= LARGE_BANNER.getHeight() && width >= LARGE_BANNER.getWidth()) {
+            return LARGE_BANNER;
         } else if (height >= LEADERBOARD.getHeight() && width >= LEADERBOARD.getWidth()) {
             return LEADERBOARD;
         } else if (height >= FULL_BANNER.getHeight() && width >= FULL_BANNER.getWidth()) {
